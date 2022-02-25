f11565019261459bd76c658e81f94af09b8b7fe6
Satyawan_Hajare <satyawan.hajare@capgemini.com>
2022-02-25 16:55:07 +0530

    Remove deprecated code

--- app/src/main/java/com/cg/marvel_app/data/characters/CharacterData.kt
+++ app/src/main/java/com/cg/marvel_app/data/characters/CharacterData.kt
@@ -2,7 +2,7 @@ package com.cg.marvel_app.data.characters
 
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class CharacterData(val results: List<CharacterResult>) : Parcelable
--- app/src/main/java/com/cg/marvel_app/data/characters/CharacterResponse.kt
+++ app/src/main/java/com/cg/marvel_app/data/characters/CharacterResponse.kt
@@ -1,7 +1,7 @@
 package com.cg.marvel_app.data.characters
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class CharacterResponse(val data: CharacterData) : Parcelable
