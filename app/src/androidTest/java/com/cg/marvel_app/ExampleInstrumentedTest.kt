df3cecdaab12b2af2cfd607784effc295b1b9c16
Satyawan_Hajare <satyawan.hajare@capgemini.com>
2022-03-02 19:46:36 +0530

    remove unused code

--- app/src/androidTest/java/com/cg/marvel_app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/cg/marvel_app/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.cg.marvelapp", appContext.packageName)
+        assertEquals("com.cg.marvel_app", appContext.packageName)
     }
 }
